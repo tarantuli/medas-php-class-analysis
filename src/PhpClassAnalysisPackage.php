facd94c0bc20d7206176d17b0a4e2071a156007c
Patrick Morpey <patrick@morphp.nl>
2023-05-11 13:26:35 +0200

    Replaced placeholders

--- src/PhpClassAnalysisPackage.php
+++ src/PhpClassAnalysisPackage.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Medas\Placeholder;
+namespace Medas\PhpClassAnalysis;
 
 use Medas\Core\AsSingleton;
 use Medas\ServiceManager\BasePackage;
 
-class PlaceholderPackage extends BasePackage
+class PhpClassAnalysisPackage extends BasePackage
 {
     use AsSingleton;
 
